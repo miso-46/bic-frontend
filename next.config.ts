7a0ab1ec40ef5b0b39f594a50940817bbd7a6468
mukagen-mac <tech0.mukainakano@gmail.com>
2025-04-20 00:45:21 +0900

    再度解消対応

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  images: {
+    domains: ['tech0gen8step4bicstorage.blob.core.windows.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tech0gen8step4bicstorage.blob.core.windows.net',
+        port: '',
+        pathname: '/bic-storage/product/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
